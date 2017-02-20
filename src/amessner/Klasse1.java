1cfbec997914870d832892f68a7975ed4f561cbe
amessner <alexander.messner.am@gmail.com>
2017-02-20 10:05:53 +0100

    Branch01.2

--- src/amessner/Klasse1.java
+++ src/amessner/Klasse1.java
@@ -4,7 +4,8 @@ public class Klasse1 {
 
 	public static void main(String[] args) {
 		System.out.println("Branch01.1 erzeugt");
-
+		System.out.println("Branch01.2 erzeugt");
+		System.out.println("Version3 erzeugt");
 	}
 
 }
--- src/amessner/Klasse2.java
+++ src/amessner/Klasse2.java
@@ -4,6 +4,8 @@ public class Klasse2 {
 
 	public static void main(String[] args) {
 		System.out.println("Branch01.1 erzeugt");
+		System.out.println("Branch01.2 erzeugt");
+		System.out.println("Version3 erzeugt");
 	}
 
 }
