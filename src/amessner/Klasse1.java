8d59cebba95ec8f723822160b3f1d0821a135942
amessner <alexander.messner.am@gmail.com>
2017-02-20 09:57:18 +0100

    Branch02

--- src/amessner/Klasse1.java
+++ src/amessner/Klasse1.java
@@ -3,8 +3,8 @@ package amessner;
 public class Klasse1 {
 
 	public static void main(String[] args) {
-
-
+		System.out.println("Branch01 erzeugt");
+		System.out.println("Branch02 erzeugt");
 	}
 
 }
--- src/amessner/Klasse2.java
+++ src/amessner/Klasse2.java
@@ -3,7 +3,8 @@ package amessner;
 public class Klasse2 {
 
 	public static void main(String[] args) {
-		
+		System.out.println("Branch01 erzeugt");
+		System.out.println("Branch02 erzeugt");
 	}
 
 }
